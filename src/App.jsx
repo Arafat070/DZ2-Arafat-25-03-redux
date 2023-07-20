b626e20a1253a359d62b186f9d6dedabcce27bb4
Arafat070 <deud129@gmail.com>
2023-07-20 23:01:37 +0600

    Создал две страницы на одной странице вывел посты через axios а во второй странице создал для создания поста все вроде работает

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import "./App.css";
+import NewPost from "./pages/NewPost/NewPost"
+import Mainpost from "./pages/MainPost/MainPost"
+
+function App() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Mainpost />} />
+                <Route path="/new-post" element={<NewPost />} />
+            </Routes>
+        </>
+    );
+}
+
+export default App;
